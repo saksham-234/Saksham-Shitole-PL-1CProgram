fb39161dddaa2d2b569906993a262414ac57e18c
saksham-234 <sakshamshitole50@gmail.com>
2026-09-01 09:38:36 -0700

    Add PL-1 Program

---
+++ sss1.c
@@ -0,0 +1,23 @@
+/* 
+Program(1)- Write a program to make use of basic Input/Output functions using different data types 
+Solution(1)- Predefined Values.
+*/
+
+#include<stdio.h>
+int main()
+{
+int rollnum;
+float per;
+char grade;
+
+rollnum=55;
+per=88.88;
+grade='A';
+
+printf("......Studet Information......");
+printf("\n Roll Number:%d",rollnum);
+printf("\n Percentage:%f",per);
+printf("\n Grade     :%C",grade);
+
+return 0;
+}
